d5567f3fffad77192dca820637f636eb9119f6fc
hyunglee2 <hihylee2@gmail.com>
2024-12-02 21:09:52 +0900

    Revert "[Feat] router setup"
    This reverts commit d968ad7ac314de9ad11bd5c52024bf2e6c377fc8.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
-import "./styles/App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
-import BookPage from "./pages/book/BookPage";
-import CalendarPage from "./pages/calendar/CalendarPage";
-import LoginPage from "./pages/login/LoginPage";
-import MainPage from "./pages/main/MainPage";
-import NicknamePage from "./pages/Nickname/NicknamePage";
-import AddPage from "./pages/register/add/AddPage";
-import SuccessPage from "./pages/register/success/SuccessPage";
+import logo from './assets/logo.svg';
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/Main" element={<MainPage />} />
-          <Route path="/BookPage" element={<BookPage />} />
-          <Route path="/CalendarPage" element={<CalendarPage />} />
-          <Route path="/LoginPage" element={<LoginPage />} />
-          <Route path="/NicknamePage" element={<NicknamePage />} />
-          <Route path="/Register/AddPage" element={<AddPage />} />
-          <Route path="/Register/SuccessPage" element={<SuccessPage />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
