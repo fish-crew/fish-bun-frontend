ec76a35aa75f169457d9d819dca61016e156a575
hyunglee2 <hihylee2@gmail.com>
2024-12-05 20:01:01 +0900

    [Fix] Header, Routes, Footer 배치 수정
    Header: 화면 상단에 고정.
    Routes: 중앙에서 동적으로 남은 공간을 차지.
    Footer: 화면 하단에 고정.

--- src/App.js
+++ src/App.js
@@ -12,18 +12,21 @@ import SuccessPage from "./pages/register/success/SuccessPage";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App flex flex-col h-screen justify-between">
       <BrowserRouter>
         <Header />
-        <Routes>
-          <Route path="/main" element={<MainPage />} />
-          <Route path="/bookPage" element={<BookPage />} />
-          <Route path="/calendarPage" element={<CalendarPage />} />
-          <Route path="/loginPage" element={<LoginPage />} />
-          <Route path="/nicknamePage" element={<NicknamePage />} />
-          <Route path="/register/addPage" element={<AddPage />} />
-          <Route path="/register/successPage" element={<SuccessPage />} />
-        </Routes>
+        {/* Routes를 감싸는 div에 flex-grow 적용 */}
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/main" element={<MainPage />} />
+            <Route path="/bookPage" element={<BookPage />} />
+            <Route path="/calendarPage" element={<CalendarPage />} />
+            <Route path="/loginPage" element={<LoginPage />} />
+            <Route path="/nicknamePage" element={<NicknamePage />} />
+            <Route path="/register/addPage" element={<AddPage />} />
+            <Route path="/register/successPage" element={<SuccessPage />} />
+          </Routes>
+        </div>
         <Footer />
       </BrowserRouter>
     </div>
