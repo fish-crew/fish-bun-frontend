7a72df659b4db60b4dd4fa578ca9b7cabb35e35d
youjeong Jeong <wjddbwjd1205@naver.com>
2024-12-06 18:47:45 +0900

    [Feat] 달력에서 '일' 제거하고 숫자만 표시하는 기능 추가
    - moment 라이브러리를 사용하여 날짜 포맷 변경
    - 숫자만 표시되도록 formatDay 로직 수정

--- src/pages/calendar/CalendarPage.js
+++ src/pages/calendar/CalendarPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import moment from "moment";
 
 function CalendarPage() {
   const [value, setValue] = useState(new Date());
@@ -13,7 +14,11 @@ function CalendarPage() {
   return (
     <div className="flex flex-col w-full h-screen">
       <div className="flex-grow flex items-center justify-center">
-        <Calendar onChange={handleDateChange} value={value} />
+        <Calendar
+          onChange={handleDateChange}
+          value={value}
+          formatDay={(locale, date) => moment(date).format('D')}
+        />
       </div>
     </div>
   );
