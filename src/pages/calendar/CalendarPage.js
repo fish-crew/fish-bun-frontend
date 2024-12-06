a7e0bc3569c6573aab0dcee1e43274527d37fe30
youjeong Jeong <wjddbwjd1205@naver.com>
2024-12-06 20:03:29 +0900

    [Feat] 년월을 월만 표시하도록 변경하고, 요일에 기본 적용되어있던 밑줄 삭제

--- src/pages/calendar/CalendarPage.js
+++ src/pages/calendar/CalendarPage.js
@@ -2,22 +2,27 @@ import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import moment from "moment";
+import styles from './CalendarPage.module.css'; // CSS 모듈 불러오기
 
 function CalendarPage() {
   const [value, setValue] = useState(new Date());
 
   const handleDateChange = (date) => {
-    setValue(date);
-    console.log('선택된 날짜:', date);
+    const formattedDate = moment(date).format("YYYY-MM-DD")
+    setValue(formattedDate);
+    console.log('선택된 날짜:', formattedDate);
   };
 
   return (
     <div className="flex flex-col w-full h-screen">
       <div className="flex-grow flex items-center justify-center">
         <Calendar
+          //locale="en"
           onChange={handleDateChange}
           value={value}
+          className={styles.reactCalendar} // CSS 모듈로 커스텀 클래스 적용
           formatDay={(locale, date) => moment(date).format('D')}
+          formatMonthYear={(locale, date) => `${moment(date).format('MM')}월`}
         />
       </div>
     </div>
