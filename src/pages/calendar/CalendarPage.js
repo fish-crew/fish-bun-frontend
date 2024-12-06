ce7d9f9b637b6eaf9de66742a2b8fd66fc7757d0
youjeong Jeong <wjddbwjd1205@naver.com>
2024-12-07 01:14:49 +0900

    [Design] 캘린더 페이지 레이아웃 및 스타일 조정
    - 캘린더의 내비게이션 버튼(<, >) 숨김
    - 캘린더 주변 구조 정리를 위해 Flexbox 조정

--- src/pages/calendar/CalendarPage.js
+++ src/pages/calendar/CalendarPage.js
@@ -15,21 +15,27 @@ function CalendarPage() {
 
   return (
     <div className="flex flex-col w-full h-screen overflow-auto">
-      <div class="text-2xl items-center m-6">붕어 탐험일지</div>
-      <div className="flex-grow flex items-center justify-center mx-6">
-        <Calendar
-          //locale="en"
-          onChange={handleDateChange}
-          value={value}
-          className={styles.reactCalendar} // CSS 모듈로 커스텀 클래스 적용
-          formatDay={(locale, date) => moment(date).format('D')}
-          formatMonthYear={(locale, date) => `${moment(date).format('MM')}월`}
-          showNeighboringMonth={false} // 인접 월의 날짜 표시 안 함
-          next2Label={null} // ">>" 버튼 숨기기
-          prev2Label={null} // "<<" 버튼 숨기기
-        />
+      <div>
+        <div className="text-2xl items-center m-6">붕어 탐험일지</div>
+        <div className="grow-0 items-start justify-center mx-6">
+          <Calendar
+            //locale="en"
+            onChange={handleDateChange}
+            value={value}
+            className={styles.reactCalendar} // CSS 모듈로 커스텀 클래스 적용
+            formatDay={(locale, date) => moment(date).format('D')}
+            showNeighboringMonth={false} // 인접 월의 날짜 표시 안 함
+            next2Label={null} // ">>" 버튼 숨기기
+            prev2Label={null} // "<<" 버튼 숨기기
+            prevLabel={null} // "<" 버튼 숨기기
+            nextLabel={null} // ">" 버튼 숨기기
+            navigationLabel={({ date }) => (
+              <span className="text-lg font-bold">{moment(date).format('MM')}월</span>
+            )}
+          />
+        </div>
+        <div className="text-xl items-center m-6">이번달은 n마리의 붕어빵을 먹었어요!</div>
       </div>
-      <div class="text-xl items-center m-6">이번달은 n마리의 붕어빵을 먹었어요!</div>
     </div>
   );
 }
