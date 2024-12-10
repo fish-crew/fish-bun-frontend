9feafff63b13416d116076a9d4e0f10d851df202
youjeong Jeong <wjddbwjd1205@naver.com>
2024-12-10 13:50:25 +0900

    [feat] Tailwind 설정에 커스텀 폰트 및 폰트 크기 추가
    - HakgyoansimGeurimilgiTTF-R 및 KyoboHand 폰트 추가
    - title, sz25, sz30, sz35, sz40, sz45, lg 등 커스텀 폰트 크기 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,19 @@ module.exports = {
       spacing: {
         dvh: "100dvh",
       },
+      fontFamily: {
+        hakgyo: ['HakgyoansimGeurimilgiTTF-R', 'sans-serif'],
+        kyoboHand: ['KyoboHand', 'sans-serif'],
+      },
+      fontSize: {
+        title: '5dvh',
+        sz25: '2.5dvh',
+        sz30: '3dvh',
+        sz35: '3.5dvh',
+        sz40: '4dvh',
+        sz45: '4.5dvh',
+        lg: '3dvh'
+      },
     },
   },
   plugins: [],
