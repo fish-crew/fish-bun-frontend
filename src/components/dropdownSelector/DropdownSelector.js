46b8734bd16d389527f921d7aaf018a2b02b4532
hyunglee2 <hihylee2@gmail.com>
2024-12-12 03:16:32 +0900

    [design] 아래 화살표 svg 변경

--- src/components/dropdownSelector/DropdownSelector.js
+++ src/components/dropdownSelector/DropdownSelector.js
@@ -38,7 +38,18 @@ function DropdownSelector({ options, onSelect }) {
               isOpen ? "rotate-180" : "rotate-0"
             }`}
           >
-            ▼
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="size-6 bg-[#630000] rotate-90"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
+                clip-rule="evenodd"
+              />
+            </svg>
           </span>
         </div>
       </button>
