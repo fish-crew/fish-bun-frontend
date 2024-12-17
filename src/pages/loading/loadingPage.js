fc8eed26a118e71ae1211d423cb3b69fbfef4250
hyunglee2 <hihylee2@gmail.com>
2024-12-17 18:59:52 +0900

    [feat] 로딩 시 WebP 지원 여부에 따라 모든 배경이미지 미리 로드해두기 (최소 1.5초 지연)

--- src/pages/loading/loadingPage.js
+++ src/pages/loading/loadingPage.js
@@ -1,19 +1,72 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import loadingIllust from "../../assets/loadingIllust.jpg";
 
-function LoadingPage() {
+// 이미지 목록 반환
+const getImageList = (isWebPSupported) => {
+  return isWebPSupported
+    ? [
+        "/assets/webp/loginIllust.webp",
+        "/assets/webp/bg-paperptexture-wh.webp",
+        "/assets/webp/bg-paperptexture.webp",
+        "/assets/webp/bulbBtm.webp",
+        "/assets/webp/bulbFull.webp",
+        "/assets/webp/bun-frame.webp",
+        "/assets/webp/checkPattern.webp",
+        "/assets/webp/glitter.webp",
+        "/assets/webp/paperOnCheck.webp",
+        "/assets/webp/paperOnBlueCheckT.webp",
+        "/assets/webp/paperOnCheckT.webp",
+        "/assets/webp/paperOnCheckB.webp",
+      ]
+    : [
+        "../../assets/loginIllust.png",
+        "../../assets/bg-paperptexture-wh.png",
+        "../../assets/bg-paperptexture.png",
+        "../../assets/bulbBtm.png",
+        "../../assets/bulbFull.png",
+        "../../assets/bun-frame.png",
+        "../../assets/checkPattern.png",
+        "../../assets/glitter.png",
+        "../../assets/paperOnCheck.png",
+        "../../assets/paperOnBlueCheckT.png",
+        "../../assets/paperOnCheckT.png",
+        "../../assets/paperOnCheckB.png",
+      ];
+};
+
+// 이미지 미리 로드 함수
+const preloadImages = (images) => {
+  return Promise.all(
+    images.map((src) => {
+      return new Promise((resolve) => {
+        const img = new Image();
+        img.src = src;
+        img.onload = resolve;
+        img.onerror = resolve;
+      });
+    })
+  );
+};
+
+function LoadingPage({ isWebPSupported }) {
   const navigate = useNavigate();
 
   useEffect(() => {
-    // 3초 후에 /loginPage로 이동
-    const timer = setTimeout(() => {
-      navigate("/loginPage");
-    }, 1500);
+    const images = getImageList(isWebPSupported);
+    const minimumDelay = new Promise(
+      (resolve) => setTimeout(resolve, 1500) // 최소 1.5초 유지
+    );
 
-    // 컴포넌트 언마운트 시 타이머 클리어
-    return () => clearTimeout(timer);
-  }, [navigate]);
+    Promise.all([preloadImages(images), minimumDelay])
+      .then(() => {
+        navigate("/loginPage");
+      })
+      .catch((error) => {
+        console.error("Error preloading images:", error);
+        navigate("/loginPage");
+      });
+  }, [isWebPSupported, navigate]);
 
   return (
     <div
