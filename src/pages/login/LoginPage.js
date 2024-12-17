018d01cb6239f1b49db7325857e4e524ea25349e
hyunglee2 <hihylee2@gmail.com>
2024-12-18 04:05:24 +0900

    [fix] 로그인 페이지 WebP로 교체

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -1,26 +1,17 @@
 import React from "react";
-import kakaoLoginImg from "../../assets/kakao_login_large_wide.png";
 
 function LoginPage() {
-  // WebP 지원 여부 확인
-  const isWebPSupported = document.documentElement.classList.contains("webp");
-
-  // 배경 이미지 경로 설정
-  const backgroundImage = isWebPSupported
-    ? "/assets/webp/loginIllust.webp" // WebP 지원 시
-    : require("../../assets/loginIllust.png"); // PNG 사용 시
-
   return (
     <div
       className="flex flex-col justify-between h-full items-center bg-cover bg-center relative"
       style={{
-        backgroundImage: `url(${backgroundImage})`,
+        backgroundImage: `url(/assets/webp/loginIllust.webp)`, // WebP 배경 이미지
       }}
     >
       <button className="flex justify-center absolute bottom-[15%] w-[80%] drop-shadow-smGray">
         <a href="/api/oauth2/authorization/kakao">
           <img
-            src={kakaoLoginImg}
+            src="/assets/webp/kakao_login_large_wide.webp" // WebP 카카오 로그인 버튼
             alt="카카오 로그인 버튼"
             className="w-full h-full object-cover"
           />
