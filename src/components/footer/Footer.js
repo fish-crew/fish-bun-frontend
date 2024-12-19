dc8ae6147483d95d3937dcc8a7b5da039b3c9e19
hyunglee2 <hihylee2@gmail.com>
2024-12-19 14:29:55 +0900

    Reapply "[fix] 구글 애드 2차"
    This reverts commit 575f3bdbd696522095ee0e10e6a717f8f33b9885.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,13 @@
+import GoogleAdvertise from "../ads/GoogleAdvertise";
+
 function Footer() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <div class=" mx-auto bg-gray-200">
+        <GoogleAdvertise />
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
