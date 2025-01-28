d74962a62bb741b862a96c5b14f497e4eafb905d
hyunglee2 <hihylee2@gmail.com>
2025-01-28 18:24:40 +0900

    [fIX] 모달 컴포넌트 수정

--- src/components/modals/Modal.js
+++ src/components/modals/Modal.js
@@ -1,5 +1,4 @@
-// Modal.jsx
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import styles from "./Modal.module.css";
 
@@ -10,17 +9,40 @@ const Modal = ({ isOpen, onClose, children, title }) => {
     <div className={styles.modalOverlay} onClick={onClose}>
       <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
         <div className={styles.modalHeader}>
-          <h2>{title}</h2>
+          <div className="title text-sz35 font-semibold text-[#650000]">
+            {title}
+          </div>
           <button className={styles.closeButton} onClick={onClose}>
-            &times;
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6 stroke-[1.5px]"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
           </button>
         </div>
         <div className={styles.modalBody}>
-          {Array.isArray(children)
-            ? children.map((child, index) => (
+          {typeof children === "string" ? (
+            <div dangerouslySetInnerHTML={{ __html: children }} />
+          ) : Array.isArray(children) ? (
+            children.map((child, index) =>
+              typeof child === "string" ? (
                 <div key={index} dangerouslySetInnerHTML={{ __html: child }} />
-              ))
-            : children}
+              ) : (
+                <div key={index}>{child}</div>
+              )
+            )
+          ) : (
+            children
+          )}
         </div>
       </div>
     </div>
@@ -30,7 +52,12 @@ const Modal = ({ isOpen, onClose, children, title }) => {
 Modal.propTypes = {
   isOpen: PropTypes.bool.isRequired,
   onClose: PropTypes.func.isRequired,
-  children: PropTypes.oneOfType([PropTypes.node, PropTypes.array]),
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.string),
+  ]),
   title: PropTypes.string,
 };
 
