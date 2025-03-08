a85bd4ebc52ab139a5feffa127757b449afea3bb
윤현지 <yunhyeonji@MacBook-Pro-3.local>
2025-03-08 22:13:03 +0900

    fix: cookie

--- src/api/cookie.js
+++ src/api/cookie.js
@@ -1,13 +1,11 @@
 export function getCookie() {
-  // const cookies = document.cookie.split("; ");
-  // for (let cookie of cookies) {
-  //   const [key, value] = cookie.split("=");
-  //   if (key === "accessToken") {
-  //     // 고정된 key 값
-  //     return value;
-  //   }
-  // }
-  // return null;
-
-  return "2DELXrtn58mIXT1Znqm56ChcbIz9zVxDAAAAAQorDNMAAAGVdS6ol5gXPJRhmZ-F";
+  const cookies = document.cookie.split("; ");
+  for (let cookie of cookies) {
+    const [key, value] = cookie.split("=");
+    if (key === "accessToken") {
+      // 고정된 key 값
+      return value;
+    }
+  }
+  return null;
 }
