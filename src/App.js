42d9d1e5e94c92324535dbcf84b56a120967d3a4
윤현지 <yunhyeonji@MacBook-Pro-3.local>
2025-03-10 23:34:37 +0900

    fix: resolve conflit

--- src/App.js
+++ src/App.js
@@ -22,6 +22,8 @@ import DetailPage from "./pages/detail/DetailPage";
 import BungBalGamePage from "./pages/game/bungBalGamePage";
 import BungBalResultPage from "./pages/game/result/resultPage";
 import BookDetailPage from "./pages/bookDetail/BookDetailPage";
+import DebateList from "./pages/debate/DebateList";
+import DebatePost from "./pages/debate/DebatePost";
 import { Provider } from "react-redux";
 import store, { persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
@@ -115,6 +117,22 @@ function AppContent({ isWebPSupported }) {
           <Route path="store/:id" element={<StoreDetailPage />} />
         </Route>
 
+        <Route
+          path="/debateList"
+          element={
+            <ProtectedRoute>
+              <DebateList />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/debatePost/:postid"
+          element={
+            <ProtectedRoute>
+              <DebatePost />
+            </ProtectedRoute>
+          }
+        />
         {/* 잘못된 경로일 때 */}
         <Route path="*" element={<Navigate to="/main" replace />} />
       </Routes>
