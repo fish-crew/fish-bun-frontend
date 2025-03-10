8f44e5faabe8cfa44f60763c7c3793dcaf9d24ce
윤현지 <yunhyeonji@MacBook-Pro-3.local>
2025-03-10 23:36:30 +0900

    fix: resolve conflict

--- src/App.js
+++ src/App.js
@@ -22,6 +22,8 @@ import DetailPage from "./pages/detail/DetailPage";
 import BungBalGamePage from "./pages/game/bungBalGamePage";
 import BungBalResultPage from "./pages/game/result/resultPage";
 import BookDetailPage from "./pages/bookDetail/BookDetailPage";
+import DebateList from "./pages/debate/DebateList";
+import DebatePost from "./pages/debate/DebatePost";
 import { Provider } from "react-redux";
 import store, { persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
