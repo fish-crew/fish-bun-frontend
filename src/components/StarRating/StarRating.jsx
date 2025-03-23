821a6a6d5c1a69388e5a6384788a4ddbb8412484
youjeong Jeong <wjddbwjd1205@naver.com>
2025-03-23 23:49:55 +0900

    [feat] 별점 모달 기본값을 myRating 기준으로 설정

--- src/components/StarRating/StarRating.jsx
+++ src/components/StarRating/StarRating.jsx
@@ -8,7 +8,7 @@ const StarRating = ({ avgRating, flavorId, refreshData, myRating }) => {
   const [isModalOpen, setIsModalOpen] = useState(false); // 모달 상태
 
   const openModal = () => {
-    setTempRating(rating); // 기존 별점 유지
+    setTempRating(myRating ?? 0); // 기존 별점 유지
     setIsModalOpen(true);
   };
 
